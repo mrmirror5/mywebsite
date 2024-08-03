28078083ddb3d7ccc8b722576fd7a44d860c2431
mrmirror5 <mirstumiro@gmail.com>
2024-08-03 18:41:02 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 
 
+document.getElementById("alertButton").addEventListener('click', function() {
+    document.reload()
+  });
 
 document.getElementById("alertButton").addEventListener('click', function() {
     alert('Painoit nappia!');
