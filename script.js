cdf535a94b45de336c14eef151993c19b724ad73
mrmirror5 <mirstumiro@gmail.com>
2024-08-06 16:47:49 +0300

    new paragraph and image in index.html

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("refreshButton").addEventListener('click', function() {
+  location.reload()
+});
+
+document.getElementById("alertButton").addEventListener('click', function() {
+  alert('Painoit nappia!');
+});
