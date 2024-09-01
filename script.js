a947f8918547cb3acb67f73565383e27ee336643
mrmirror5 <mirstumiro@gmail.com>
2024-09-01 21:24:54 +0300

    fixed bug on color thing

--- script.js
+++ script.js
@@ -12,17 +12,21 @@ const leftButton =document.getElementById("scroll-left");
 const rightButton =document.getElementById("scroll-right");
 const body = document.getElementById("the_body");
 
-  
+// Nuolet ohjaavat väriä ja vaihtavat punaisen arvoa!  
 let red = 255
 let green = 244
 let blue = 149
 
 leftButton.addEventListener("click", function(){
-  red = red-10
+  if (red>0){
+    red = red-10
+  }
   body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
 })
 
 rightButton.addEventListener("click", function(){
-  red = red+10
+  if (red<=255){
+    red = red+10
+  }
   body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
 })
