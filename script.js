9837769ed8f990a35cabc072e0d3e3054041a295
mrmirror5 <mirstumiro@gmail.com>
2024-09-01 21:13:13 +0300

    buttons nowchange bg-colour

--- script.js
+++ script.js
@@ -5,3 +5,24 @@ document.getElementById("refreshButton").addEventListener('click', function() {
 document.getElementById("alertButton").addEventListener('click', function() {
   alert('Painoit nappia!');
 });
+
+
+
+const leftButton =document.getElementById("scroll-left");
+const rightButton =document.getElementById("scroll-right");
+const body = document.getElementById("the_body");
+
+  
+let red = 255
+let green = 244
+let blue = 149
+
+leftButton.addEventListener("click", function(){
+  red = red-10
+  body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+})
+
+rightButton.addEventListener("click", function(){
+  red = red+10
+  body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+})
