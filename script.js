0090f522c4f1814a33d33550596f0fd05703aab7
mrmirror5 <mirstumiro@gmail.com>
2024-09-07 14:35:50 +0300

    added header and logo

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ document.getElementById("alertButton").addEventListener('click', function() {
 const leftButton =document.getElementById("scroll-left");
 const rightButton =document.getElementById("scroll-right");
 const body = document.getElementById("the_body");
+const header = document.querySelector(".header");
 
 // Nuolet ohjaavat väriä ja vaihtavat punaisen arvoa!  
 let red = 255
@@ -22,6 +23,7 @@ leftButton.addEventListener("click", function(){
     red = red-10
   }
   body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+  /*header.style.backgroundColor = `rgb(${red}, ${green-150}, ${blue-15})`;*/
 })
 
 rightButton.addEventListener("click", function(){
