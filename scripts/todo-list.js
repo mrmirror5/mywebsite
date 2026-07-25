99406d78babf1804ffe40a52f4cf9a7502cf9bdb
mrmirror5 <mirstumiro@gmail.com>
2026-07-25 19:51:16 +0300

    Added weather app to portfolio

--- scripts/todo-list.js
+++ scripts/todo-list.js
@@ -7,6 +7,11 @@ const projectsArray = [{
     link: "/todo-list"
 },
 {
+    name: "Weather App",
+    description: "A nice weather app that displays a city's temperature, wind and shows a widget.",
+    link: "#coming soon"
+},
+{
     name: "World Cup Game Info",
     description: "I programmed a nice react app that fetches it's data incluiding: match info, score and more from a football api, and displays it in a nice format.",
     link: "https://mrmirror5.github.io/world-cup-stats/"
@@ -14,7 +19,7 @@ const projectsArray = [{
 {
     name: "To-Do List v2",
     description: "A simple to-do-list app that is made with React and utilizes the Mantine UI library for styling.",
-    link: "https://google.com"
+    link: "https://mrmirror5.github.io/react-todo-app/"
 }
 ]
 
